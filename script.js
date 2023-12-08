64a00707edf8171689167012f98d4d02bd9367d1
SametKiybik <samettemas05@gmail.com>
2023-12-08 22:14:50 +0300

    sliders

---
+++ script.js
@@ -0,0 +1,7 @@
+function grayscaleMouseOver() {
+    document.querySelector('.sliderC').style.filter = 'grayscale(0%)';
+}
+
+function grayscaleMouseOut() {
+    document.querySelector('.sliderC').style.filter = 'grayscale(100%)';
+}
